c802ac96422184642f78155619e83f1131279498
unknown <ychsieh0725@gmail.com>
2026-04-07 16:55:52 +0800

    20260407class

--- python_basic/Module9/9-1.py
+++ python_basic/Module9/9-1.py
@@ -1,9 +1,13 @@
 # import 自己建立的模組
-
+import myModule
 
 # 計算幾次方
+result = myModule.pow(10, 2)
 
+total = myModule.get_discount(6000)
 
+print(f"9-1.py - function pow 的結果：{result}")
+print(f"9-1.py - function get-discount 的結果：{total}元。")
 # 簡單斷句
 
 
--- python_basic/Module9/myModule.py
+++ python_basic/Module9/myModule.py
@@ -1,6 +1,11 @@
 # 計算幾次方
+def pow(x, y):
+    return x ** y
 
-
+def get_discount(price):
+    if price >= 2000:
+        price -= 200*(price//2000)
+    return price
 
 # 簡單斷句
 
@@ -9,6 +14,8 @@
 
 
 
-# 測試模組
-
+# 測試模組(固定寫法，再函式庫裡都要這樣寫，才不會在import檔案裏面出現)
+if __name__ == "__main__":
+    print(f"myModule.py - function pow 的結果：{pow(2, 3)}")
+    print(f"myModule.py - function get-discount 的結果：{get_discount(6000)}元。")
 
